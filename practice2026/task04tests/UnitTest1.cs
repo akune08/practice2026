e7e94daf945e4afbd1f049068d0e17736cea0a74
Платон Орловский <Orlovsky_Platon@mail.ru>
2026-06-26 13:04:54 +0600

    Реализованы классы для игры "Космический бой"

---
+++ practice2026/task04tests/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using Xunit;
+using task04;
+
+namespace task04tests
+{
+    public class SpaceshipTests
+    {
+        [Fact]
+        public void Cruiser_ShouldHaveCorrectStats()
+        {
+            ISpaceship cruiser = new Cruiser();
+
+            Assert.Equal(50, cruiser.Speed);
+            Assert.Equal(100, cruiser.FirePower);
+        }
+
+        [Fact]
+        public void Fighter_ShouldHaveCorrectStats()
+        {
+            ISpaceship fighter = new Fighter();
+
+            Assert.Equal(100, fighter.Speed);
+            Assert.Equal(20, fighter.FirePower);
+        }
+
+        [Fact]
+        public void Fighter_ShouldBeFasterThanCruiser()
+        {
+            var fighter = new Fighter();
+            var cruiser = new Cruiser();
+
+            Assert.True(fighter.Speed > cruiser.Speed);
+        }
+
+        [Fact]
+        public void Cruiser_FirePower_ShouldBeGreaterWithRespectToFighter()
+        {
+            var fighter = new Fighter();
+            var cruiser = new Cruiser();
+
+            Assert.True(cruiser.FirePower > fighter.FirePower);
+        }
+
+        [Fact]
+        public void Ships_ShouldExecuteActionsWithoutExceptions()
+        {
+            ISpaceship cruiser = new Cruiser();
+            ISpaceship fighter = new Fighter();
+
+            var cruiserException = Record.Exception(() =>
+            {
+                cruiser.MoveForward();
+                cruiser.Rotate(90);
+                cruiser.Fire();
+            });
+
+            var fighterException = Record.Exception(() =>
+            {
+                fighter.MoveForward();
+                fighter.Rotate(-45);
+                fighter.Fire();
+            });
+
+            Assert.Null(cruiserException);
+            Assert.Null(fighterException);
+        }
+    }
+}
