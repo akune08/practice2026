92fb4f0c5c44c5a18934cd1a090adc73e717d83f
Платон Орловский <Orlovsky_Platon@mail.ru>
2026-07-03 18:02:59 +0600

    Реализован калькулятор

---
+++ practice2026/task11/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Task11
+{
+    public interface ICalculator
+    {
+        int Add(int a, int b);
+        int Minus(int a, int b);
+        int Mul(int a, int b);
+        int Div(int a, int b);
+    }
+
+    public static class CalculatorEngine
+    {
+        public static ICalculator CreateCalculator()
+        {
+            string classCode = @"
+            using Task11;
+            public class Calculator : ICalculator
+            {
+                public int Add(int a, int b) => a + b;
+                public int Minus(int a, int b) => a - b;
+                public int Mul(int a, int b) => a * b;
+                public int Div(int a, int b) => a / b;
+            }";
+
+            var tree = SyntaxFactory.ParseSyntaxTree(classCode);
+
+            var references = new MetadataReference[]
+            {
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(ICalculator).Assembly.Location)
+            };
+
+            var compilation = CSharpCompilation.Create("DynamicCalculatorAssembly")
+                .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+                .AddReferences(references)
+                .AddSyntaxTrees(tree);
+
+            using (var ms = new MemoryStream())
+            {
+                var result = compilation.Emit(ms);
+
+                if (!result.Success)
+                {
+                    string errors = string.Join("\n", result.Diagnostics);
+                    throw new Exception($"Ошибка компиляции:\n{errors}");
+                }
+
+                var assembly = Assembly.Load(ms.ToArray());
+
+                var type = assembly.GetType("Calculator");
+                return (ICalculator)Activator.CreateInstance(type);
+            }
+        }
+    }
+}
---
+++ practice2026/task11tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+public class CalculatorTests
+{
+    [Fact]
+    public void TestCalculatorOperations()
+    {
+        ICalculator calc = DynamicCalculatorFactory.Create();
+
+        Assert.Equal(5, calc.Add(2, 3));
+        Assert.Equal(1, calc.Minus(3, 2));
+        Assert.Equal(6, calc.Mul(2, 3));
+        Assert.Equal(2, calc.Div(6, 3));
+    }
+}
