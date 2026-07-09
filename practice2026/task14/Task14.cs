5275e562e4a349a7319aa49cf30e22cdc4aa1ef5
Платон Орловский <Orlovsky_Platon@mail.ru>
2026-07-09 12:06:34 +0600

    Реализовано вычисление определённого интеграла методом трапеции

---
+++ practice2026/task14/Task14.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+class DefiniteIntegral
+{
+    public static double Solve(double a, double b, Func<double, double> function, double step, int threadsNumber)
+    {
+        double totalIntegral = 0.0;
+        double range = (b - a) / threadsNumber;
+
+        Barrier barrier = new Barrier(threadsNumber, (brier) => {
+        });
+
+        Thread[] threads = new Thread[threadsNumber];
+
+        for (int i = 0; i < threadsNumber; i++)
+        {
+            int index = i; 
+            threads[i] = new Thread(() =>
+            {
+                double start = a + index * range;
+                double end = start + range;
+                double localSum = 0.0;
+
+                for (double x = start; x < end; x += step)
+                {
+                    localSum += (function(x) + function(Math.Min(x + step, end))) * step / 2.0;
+                }
+
+                AddToTotal(ref totalIntegral, localSum);
+
+                barrier.SignalAndWait();
+            });
+            threads[i].Start();
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+
+        return totalIntegral;
+    }
+
+    private static void AddToTotal(ref double location, double value)
+    {
+        double initialValue, newValue;
+        do
+        {
+            initialValue = location;
+            newValue = initialValue + value;
+        }
+        while (Interlocked.CompareExchange(ref location, newValue, initialValue) != initialValue);
+    }
+}
---
+++ practice2026/task14tests/Task14tests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+using System;
+
+public class DefiniteIntegralTests
+{
+    [Fact]
+    public void Solve_LinearFunction_ReturnsCorrectResult()
+    {
+        var X = (double x) => x;
+        double result = DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2);
+        Assert.Equal(0, result, 4); 
+    }
+
+
+    [Fact]
+    public void Solve_SinFunction_ReturnsCorrectResult()
+    {
+        var SIN = (double x) => Math.Sin(x);
+        double result = DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8);
+        Assert.Equal(0, result, 4);
+    }
+
+    [Fact]
+    public void Solve_LinearRange0to5_ReturnsCorrectResult()
+    {
+        var X = (double x) => x;
+        double result = DefiniteIntegral.Solve(0, 5, X, 1e-6, 8);
+        Assert.Equal(12.5, result, 5);
+    }
+
+    [Fact]
+    public void Solve_ConsistencyAcrossThreadCounts()
+    {
+        var square = (double x) => x * x;
+        double res1 = DefiniteIntegral.Solve(0, 2, square, 1e-4, 1);
+        double res4 = DefiniteIntegral.Solve(0, 2, square, 1e-4, 4);
+
+        Assert.Equal(res1, res4, 3);
+    }
+}
