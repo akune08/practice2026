f5a7c4fd531da11e40455e7de4f5e7f9ff973728
akune08 <orlovsky_platon@mail.ru>
2026-07-15 21:49:59 +0600

    Выполнена задача 18

---
+++ task17/CommandProcessor/CommandProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Concurrent;
+using System.Windows.Input;
+
+public class CommandProcessor
+{
+    private readonly IScheduler _scheduler;
+    private readonly BlockingCollection<ICommand> _incomingQueue; 
+
+    public void Run()
+    {
+        while (true)
+        {
+            if (_incomingQueue.TryTake(out var newCmd, 0))
+            {
+                _scheduler.Add(newCmd);
+            }
+
+            if (_scheduler.HasCommand())
+            {
+                var cmd = _scheduler.Select();
+                cmd.Execute(); 
+            }
+            else
+            {
+                var cmd = _incomingQueue.Take();
+                _scheduler.Add(cmd);
+            }
+        }
+    }
+}
